5227fcc04bfdc05bb497297ed38351c3d0b12055
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-02-13 13:24:30 -0600

    Oops.

--- docker_launch.sh
+++ docker_launch.sh
@@ -6,6 +6,6 @@ cd data
 java \
 	-XX:+CrashOnOutOfMemoryError -XX:+AlwaysPreTouch -XX:+UseNUMA \
 	-XX:+ExplicitGCInvokesConcurrent -XX:+UseCompressedOops \
-	-XX:+UseShenandoahGC XX:+ShenandoahPacing -XX:+ShenandoahDegeneratedGC \
+	-XX:+UseShenandoahGC -XX:+ShenandoahPacing -XX:+ShenandoahDegeneratedGC \
 	-XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 \
 	-jar ../flux.jar
