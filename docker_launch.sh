4c6ae124fea9a6cd192fccb14ae94939ce52917c
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-02-13 13:29:08 -0600

    Sigh.

--- docker_launch.sh
+++ docker_launch.sh
@@ -6,6 +6,6 @@ cd data
 java \
 	-XX:+CrashOnOutOfMemoryError -XX:+AlwaysPreTouch -XX:+UseNUMA \
 	-XX:+ExplicitGCInvokesConcurrent -XX:+UseCompressedOops \
-	-XX:+UseShenandoahGC -XX:+UnlockExperimentalVMOptions -XX:+ShenandoahPacing -XX:+ShenandoahDegeneratedGC \
+	-XX:+UseShenandoahGC -XX:+UnlockExperimentalVMOptions -XX:+ShenandoahPacing \
 	-XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 \
 	-jar ../server.jar
